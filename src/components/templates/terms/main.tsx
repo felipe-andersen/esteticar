d855c2ff4bd9743da54cc69b73320b5996bc43b3
felipe <felipe.coutinho.psl@gmail.com>
2024-06-06 18:14:33 -0300

    add component to template terms

---
+++ src/components/templates/terms/main.tsx
@@ -0,0 +1,32 @@
+
+'use client'
+import Accordion from 'react-bootstrap/Accordion';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { result } from '@/components/organisms';
+import { X, Bell, MessageSquareIcon, MoveRight, EllipsisVertical, Key, Table, List, Filter, ChevronDown, ChevronUp} from "lucide-react";
+import VerticalMenu from '@/components/organisms/VerticalMenu';
+import { useState, useRef } from 'react';
+import Button from 'react-bootstrap/Button';
+import Overlay from 'react-bootstrap/Overlay';
+import Tooltip from 'react-bootstrap/Tooltip';
+import { TopBar } from '@/components/organisms/topBar';
+import { FaSquareBehance } from 'react-icons/fa6';
+import { CgAdd } from 'react-icons/cg';
+
+export function Main() {
+
+    return (
+
+        <main className=' wwinf bg-[#f6f8fa]  scroll ml-80 mt-16 overflow-scroll '>
+
+            terms
+
+        </main>
+
+    );
+}
+
+
+  
