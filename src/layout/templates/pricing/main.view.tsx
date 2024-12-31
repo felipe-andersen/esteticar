ac461626cc6d679a31ecd08b2992281ef26068f7
Felipe <felipe.coutinho.psl@gmail.com>
2024-12-30 22:29:02 -0300

    update

--- src/layout/templates/pricing/main.view.tsx
+++ src/layout/templates/pricing/main.view.tsx
@@ -1,14 +1,15 @@
 'use client'
-import { Check, TableCellsSplit } from "lucide-react"
+import { Check, CreditCard, TableCellsSplit } from "lucide-react"
 import Link from "next/link"
 import { useRouter } from "next/navigation"
+import { FaPix } from "react-icons/fa6"
 
 
 
 export function Main() {
     const router = useRouter()
     return (
-        <div className=" bg-neutral-100">
+        <div className=" bg-neutral-100 ">
             <main className=' h-full w-full mt-16 p-6 gap-3 flex flex-col'>
                 <div className="flex flex-col  p-6 text-center items-center gap-3">
                     <h3 className="text-2xl">
@@ -38,68 +39,229 @@ export function Main() {
                         Customers on Standard see up to 24x ROI.* Start scaling faster with advanced automations and data-driven optimization.
                     </p>
                 </div>
-                <div className="flex flex-col max-w-[1100px]">
+                <div className="flex flex-col  w-full bg-red-100 flex items-center">
 
-                <div  className="flex gap-3 ">
-                    <div className="w-[500px] p-6 border bg-white rounded-2xl">
-                        <h2 className="h-8 font-semibold">Seu carrinho</h2>
+                <div  className="flex gap-3 w-full justify-center">
+                    <div className="w-full max-w-[700px] p-6 border bg-white rounded-2xl">
+                        <h2 className="h-8 font-semibold text-xl">
+                            Item
+                        </h2>
                         <hr className="my-1"/>
-                        <div className="mb-3 mt-4">
-                            Plano Basic - Solução ideal para iniciantes   
+                        <div className="flex justify-between items-center">
+                            <div className="text-sm ">
+                                Plano Basic (12 meses) - Solução ideal para iniciantes
+                            </div>
+                            <div className="text-sm bg-green-600 text-white rounded-full p-2 px-3 w-min whitespace-nowrap font-semibold text-xs">
+                                ECONOMIZE R$1.680,00
+                            </div>
+                            <div className="text-sm  flex flex-col items-end">
+                                <span className="font-extrabold">
+                                    R$ 59,30
+                                </span>
+                                <s>
+                                    R$ 59,30
+                                </s>
+                            </div>
                         </div>
-                        <div className="border mb-3 h-10 flex w-[250px] mt-6 mb-8">
-                            <input className="h-full w-full px-2" placeholder="coupon" type="text"></input>
-                        </div>
-                        
-                        <div className="flex justify-between text-sm">
-                            <span className="text-neutral-600">Subtotal</span>
-                            <span className="font-semibold">BRL 6.99</span>
-                        </div>
+                        <span className="text-sm text-neutral-700">
+                        Renovação a R$25,99/mês em 27/02/2029. Cancele a qualquer momento!
+                        </span>
                     </div>
-                    <div className="w-[500px] p-6 border bg-white rounded-2xl">
-                        <h2 className="h-8 font-semibold">Detalhes</h2>
+                    <div className="flex flex-col p-6 bg-neutral-100">
+                        <div className="flex justify-between ">
+                            <span>
+                            Subtotal
+                            </span>
+                            <div className="flex gap-2">
+                                <span>
+                                R$2.207,52
+                                </span>
+                                <span>
+                                R$527,52
+                                </span>
+                            </div>
+                        </div>
+                        <span>O subtotal não inclui impostos aplicáveis</span>
+                        <div className="flex justify-between">
+                            <span>
+                            Desconto do Plano -76%
+                            </span>
+                            <span>
+                            -R$1.680,00
+                            </span>
+                        </div>
+                        <span className="text-sm">
+                        Tem um cupom de desconto?
+                        </span>
+                        <div className="flex gap-2">
+                            <div className=" border h-10 rounded overflow-hidden">
+                                <input
+                                    className="px-2 h-full inter " 
+                                    placeholder="input"
+                                ></input>
+                            </div>
+                            <button className=" px-2 rounded rounded h-10 bg-green-800 text-white font-semibold text-sm">
+                                enviar
+                            </button>
+                        </div>
+                        <button className="w-full rounded h-10 bg-green-800 text-white font-semibold">
+                            Continuar
+                        </button>
+                    </div>
+                    {/* <div className="w-[500px] p-6 border bg-white rounded-2xl">
+                        <h2 className="h-8 font-semibold text-xl">Detalhes</h2>
                         <hr className="my-1"/>
-                        <div className=" mt-4 flex justify-between text-sm h-8 items-center bg-neutral-0">
-                            <span className="text-neutral-600"> Plano Basic (12 meses) - Solução ideal para iniciantes</span>
+                        <div className="text-xs mt-5 pl-3 border-l-[2px] border-neutral-900 text-green-00">
+                            Escolha um método de pagamento para continuar. Você ainda terá uma chance de revisar e alterar seu pedido antes que ele seja finalizado.
+                        </div>
+                        <div className=" mt-4 flex justify-between text-sm  bg-neutral-0 text-neutral-700 items-start mb-1">
+                            <span className=""> Plano Basic (12 meses) - Solução ideal para iniciantes fdfdfdfdfdfdfdfffffffffffgkihj</span>
+                            <span className="font-semibold text-xs whitespace-nowrap">BRL 6.99</span>
+                        </div>
+                         <div className="flex justify-between text-sm h-8 items-center bg-neutral-0 text-neutral-700">
+                            <span className=""> Plano Basic (12 meses) - Solução ideal para iniciantes</span>
                             <span className="font-semibold text-xs">BRL 6.99</span>
                         </div>
-                        <div className="flex justify-between text-sm h-8 items-center bg-neutral-0">
-                            <span className="text-neutral-600"> Plano Basic - Solução ideal para iniciantes</span>
+                        <div className="flex justify-between text-sm h-8 items-center bg-neutral-0 text-neutral-700">
+                            <span className=""> Plano Basic (12 meses) - Solução ideal para iniciantes</span>
                             <span className="font-semibold text-xs">BRL 6.99</span>
                         </div>
-                        <div className="flex justify-between text-sm h-8 items-center bg-neutral-0">
-                            <span className="text-neutral-600"> Plano Basic - Solução ideal para iniciantes</span>
-                            <span className="font-semibold text-xs">BRL 6.99</span>
-                        </div>
-                        <div className="flex justify-between  h-8 items-center bg-neutral-0 my-4">
-                            <span className="text-neutral-900 font-semibold text-xs">Total</span>
+                        <div className="flex justify-between  h-8 items-center bg-neutral-0 mt-4">
+                            <span className="text-neutral-900 font-semibold text-sm">Total</span>
                             <span className="font-semibold text-sm">BRL 6.99</span>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
                 <div  className="flex gap-3 w-[500px]">
                     <div className="w-[500px] p-6 border bg-white rounded-2xl">
-                        <h2>Endereço de faturamento</h2>
-                        <div>
-                            <div>
-                                <input type="select"></input>
-                                Lorem ipson glard black
+                        <h2 className="h-8 font-semibold text-xl">
+                            Item
+                        </h2>
+                        <hr className="my-1"/>
+                        <span className="text-sm ">
+                            Cartão, pix ou carteira difital
+                        </span>
+                        <div className="flex flex-col justify-center gap-1">
+                            <div className=" text-sm flex items-center border border-green-500 rounded h-10 pr-3 max-w-[250px] ">
+                                <span className="w-12  h-full flex items-center justify-center">
+                                    <FaPix/>
+                                </span>
+                                pix
+                            </div>
+                            <div className=" text-sm flex items-center border border-green-500 rounded h-10 pr-3 max-w-[250px] ">
+                                <span className="w-12  h-full flex items-center justify-center">
+                                    <CreditCard size={18}/>
+                                </span>
+                                cartão de crédito e débito
                             </div>
                         </div>
-                        <div>fdfd</div>
-                        <div>US$ 6.99/month</div>
-                        <div>
-                        US$ 17.99/month</div>
-                    </div>
-                    <div className="w-[500px] p-6 border bg-white rounded-2xl">
-                        <h2>Resumo do pedito</h2>
-                    </div>
-                    <div className="w-[500px] p-6 border bg-white rounded-2xl">
-                        <h2>Pagamento</h2>
                     </div>
                 </div>
+                <div className="flex gap-6">
+                    <div className="w-full bg-neutral-100 p-7 border rounded-3xl bg-white min-w-[700px]">
+                        <h2 className="mb-5 font-bold text-xl">
+                            Endereço de faturamento
+                        </h2>
+                        <div className="flex gap-6 h-12 mb-5 ">
+                            <div className="h-full border h-full rounded-md overflow-hidden w-full flex items-center justify-center ">
+                                <input
+                                    className="h-full px-4 w-full text-sm h-[calc(100%-2px)] w-[calc(100%-2px)] rounded-md poppins-regular"
+                                    placeholder="Primeiro nome "
+                                />
+                            </div>
+                            <div className="h-full border h-full rounded-md overflow-hidden w-full flex items-center justify-center ">
+                                <input
+                                    className="h-full px-4 w-full text-sm h-[calc(100%-2px)] w-[calc(100%-2px)] rounded-md"
+                                    placeholder="Primeiro nome"
+                                />
+                            </div>
+                        </div>
+                    
+                        <div className="flex gap-6 h-12 w-full mb-5">
+                            <div className="h-full border h-full rounded-md overflow-hidden w-full flex items-center justify-center ">
+                                <input
+                                    className="h-full px-4 w-full text-sm h-[calc(100%-2px)] w-[calc(100%-2px)] rounded-md"
+                                    placeholder="Primeiro nome"
+                                />
+                            </div>
+                        
+                        </div>
+                        <div className="flex gap-6 h-12 w-full mb-5">
+                            <div className="h-full border h-full rounded-md overflow-hidden w-full flex items-center justify-center ">
+                                <input
+                                    className="h-full px-4 w-full text-sm h-[calc(100%-2px)] w-[calc(100%-2px)] rounded-md"
+                                    placeholder="Primeiro nome"
+                                />
+                            </div>
+                        
+                        </div>
+                        <div className="flex gap-6 h-12 mb-5 ">
+                            <div className="h-full border h-full rounded-md overflow-hidden w-full flex items-center justify-center ">
+                                <input
+                                    className="h-full px-4 w-full text-sm h-[calc(100%-2px)] w-[calc(100%-2px)] rounded-md"
+                                    placeholder="Primeiro nome"
+                                />
+                            </div>
+                            <div className="h-full border h-full rounded-md overflow-hidden w-full flex items-center justify-center ">
+                                <input
+                                    className="h-full px-4 w-full text-sm h-[calc(100%-2px)] w-[calc(100%-2px)] rounded-md"
+                                    placeholder="Primeiro nome"
+                                />
+                            </div>
+                        </div>
+                        <div className="flex gap-6 h-12 mb-5 ">
+                            <div className="h-full border h-full rounded-md overflow-hidden w-full flex items-center justify-center ">
+                                <input
+                                    className="h-full px-4 w-full text-sm h-[calc(100%-2px)] w-[calc(100%-2px)] rounded-md"
+                                    placeholder="Primeiro nome"
+                                />
+                            </div>
+                            <div className="h-full border h-full rounded-md overflow-hidden w-full flex items-center justify-center ">
+                                <input
+                                    className="h-full px-4 w-full text-sm h-[calc(100%-2px)] w-[calc(100%-2px)] rounded-md"
+                                    placeholder="Primeiro nome"
+                                />
+                            </div>
+                        </div>
+                        <div className="w-[180px] border h-12 rounded-xl overflow-hidden  flex items-center justify-center px-3 bg-green-800 text-white font-semibold">
+                            Continuar
+                        </div>
+                    </div>
+                        <div className="w-full bg-white">
+                            gfgfgfggffffffffffffffffffffffffffffffffffffffff
+                        </div>
+                    </div>
+                    <div className="flex gap-6">
+                        <div className="w-full bg-neutral-100 p-7 border rounded-3xl bg-white min-w-[700px]">
+                            <h2 className=" font-bold text-xl">
+                                Endereço de faturamento
+                            </h2>
+                            <hr className="my-1"/>
+                            <span className="text-sm ">
+                                Cartão, pix ou carteira difital
+                            </span>
+                            <div className="flex flex-col justify-center gap-1">
+                                <div className=" text-sm flex items-center border border-green-500 rounded h-10 pr-3 max-w-[250px] ">
+                                    <span className="w-12  h-full flex items-center justify-center">
+                                        <FaPix/>
+                                    </span>
+                                    pix
+                                </div>
+                                <div className=" text-sm flex items-center border border-green-500 rounded h-10 pr-3 max-w-[250px] ">
+                                    <span className="w-12  h-full flex items-center justify-center">
+                                        <CreditCard size={18}/>
+                                    </span>
+                                    cartão de crédito e débito
+                                </div>
+                            </div>
+                            Informações sobre a compra serão enviadas para o email
 
-             
+                            Dúvidas sobre segurança? sssssssssssssssssssssssAcesse o Manual de Compra Segura pre preparamos para você.
+                        </div>
+                        <div className="w-full bg-white">
+                            gfgfgfggffffffffffffffffffffffffffffffffffffffff
+                        </div>
+                    </div>
                 </div>
                 <div className="w-full py-20 justify-center items-center flex flex-col bg-green-800 gap-7">
                     <p className="text-4xl text-neutral-100 font-semibold">
